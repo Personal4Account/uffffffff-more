08d266213da46c9be97ce58cae1e6e23f9f185b0
Personal4Account <104253971+Personal4Account@users.noreply.github.com>
2022-04-24 15:48:33 +0530

    Update botinfo.py

--- strings/botinfo.py
+++ strings/botinfo.py
@@ -1,1 +1,40 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
+
+BOT_INFO = **Thanks for Joining Us 🌸
+All the information about me Given Below 🍁 Keep Supporting us ☘**
+
+
+🌟 My Owner : 
+     @Its_romeoo
+
+👨‍💻 My Maintainer :
+      • @PAPA_AADIT
+      • @xtm_BidiBaaz
+
+
+⚙ Support Chat
+     @StrayCoderSupport
+
+📺 Updates Server 
+     @StrayCoder
+
+
+🤖 Features About Me :
+      • Easy to use
+      • Powerful and Fast
+      • Cool Interface   
+      • More Optimised 
+      • Connected With YouTube
+      • working with 5 assistants
+      • Multiple Languages 
+
+🌀 Powered By :
+      @PHOENIX_EMPIRE
