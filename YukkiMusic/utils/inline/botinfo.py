d63648744c127ca30e18ecb674b66900fb27f31c
Personal4Account <104253971+Personal4Account@users.noreply.github.com>
2022-04-24 21:36:55 +0530

    Update botinfo.py

--- YukkiMusic/utils/inline/botinfo.py
+++ YukkiMusic/utils/inline/botinfo.py
@@ -23,7 +23,7 @@ def info_pannel(_, START: Union[bool, int] = None):
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data=f"settingsback_botinfo",
         ),
         InlineKeyboardButton(
             text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
