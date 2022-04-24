6f8d2163f3db9101f6332149959cf75baa7fd16d
Personal4Account <104253971+Personal4Account@users.noreply.github.com>
2022-04-24 22:17:32 +0530

    Update info.py

--- YukkiMusic/plugins/bot/info.py
+++ YukkiMusic/plugins/bot/info.py
@@ -92,7 +92,7 @@ async def help_com_group(client, message: Message, _):
 async def botinfo_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
-    keyboard = info_back_markup(_)
+    keyboard = botinfo_back_markup(_)
     try:
         await CallbackQuery.answer()
     except:
